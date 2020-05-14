02d6a784dd0602a322a39d6919c022605b4dd472
ian.bertoncelo <ian.bertoncelo@infracommerce.com.br>
2020-05-14 09:47:03 -0300

    Add forgot/reset password feature

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -30,9 +30,12 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
 
-    await this.userTokensREpository.generate(user.id);
+    const { token } = await this.userTokensREpository.generate(user.id);
 
-    this.mailProvider.sendMail(email, 'Mail body');
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de recuperação de senha recebido: ${token}`,
+    );
   }
 }
 

a995935be8e5335780fef0fc70f696a194017fb5
ian.bertoncelo <ian.bertoncelo@infracommerce.com.br>
2020-05-20 21:57:41 -0300

    Add class transformer

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from '@shared/infra/http/app';
 
 app.listen(process.env.APP_PORT, () =>
-  console.log(`Server runing on http://localhost:${process.env.APP_PORT}`),
+  console.log(`Server runing on ${process.env.APP_API_URL}`),
 );

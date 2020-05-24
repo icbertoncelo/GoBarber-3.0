0305c68f00c7f361ffc73c4a5b6f6b6d9f012c50
ian.bertoncelo <ian.bertoncelo@infracommerce.com.br>
2020-05-24 13:52:48 -0300

    Fix query params and save cache using class-transform

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, endOfDay, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -44,13 +44,19 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
+      const startAvailableDate = endOfDay(new Date(year, month - 1, day));
+
       const appointmentsInDay = appointments.filter(
         appointment => getDate(appointment.date) === day,
       );
 
+      const available =
+        isAfter(startAvailableDate, new Date()) &&
+        appointmentsInDay.length < 10;
+
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available,
       };
     });
 

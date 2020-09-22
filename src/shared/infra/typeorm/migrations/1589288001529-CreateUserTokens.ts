92b8e2ba6ba74513e6279bcc8a2464f2cc5719f5
ian.bertoncelo <ian.bertoncelo@infracommerce.com.br>
2020-09-22 10:08:40 -0300

    Fix migrations uuid

--- src/shared/infra/typeorm/migrations/1589288001529-CreateUserTokens.ts
+++ src/shared/infra/typeorm/migrations/1589288001529-CreateUserTokens.ts
@@ -3,7 +3,6 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateUserTokens1589288001529
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'user_tokens',
